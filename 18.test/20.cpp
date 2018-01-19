8c2cb7966a8777b6050a89d850c068256e4c0f3e
yangxiaox <823567085@qq.com>
2018-01-19 19:56:23 +0800

    test

---
+++ 18.test/20.cpp
@@ -0,0 +1,25 @@
+/*************************************************************************
+	> File Name: 20.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2018年01月10日 星期三 10时17分54秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+
+int main() {
+    int a = 1;
+    switch (a) {
+        default:
+            cout << "??0" << endl;
+            break;
+        case 1:
+            cout << a << endl;
+        case 2:
+            cout << "2" << endl;
+    }
+
+    return 0;
+}
