260e259eff0076def1ed186dde6d8780ca53c789
yangxiaox <823567085@qq.com>
2018-01-23 15:38:19 +0800

    test

---
+++ 18.test/27.cpp
@@ -0,0 +1,20 @@
+/*************************************************************************
+	> File Name: 27.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2018年01月23日 星期二 12时17分09秒
+ ************************************************************************/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    cout << acos(-1.0) << endl;
+    printf("%.16f\n", acos(-1.0));
+    cout << exp(1.0) << endl;
+    printf("%.16f\n", exp(1.0));
+
+
+    return 0;
+}
